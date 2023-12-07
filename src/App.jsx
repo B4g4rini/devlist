8f1fbdb513aef80804938ececfab55d2156120ce
B4g4rini <pedro.bagarini2000@gmail.com>
2023-12-07 17:07:01 -0300

    Barra de progresso e conquista realizada Adicionar e excluir tarefas realizada Editar as tarefas realizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import "./App.css"
+import CompletedTasks from "./components/CompletedTasks/CompletedTasks"
 import CreateNewTask from "./components/CreateNewTask/CreateNewTask"
 import Header from "./components/Header/Header"
-
+import Progress from "./components/Progress/Progress"
+import TodayTasks from "./components/TodayTasks/TodayTasks"
 
 
 const App = () => {
@@ -10,7 +12,10 @@ const App = () => {
     <Header/>
     <section className="content">
     <CreateNewTask/>
-
+    <TodayTasks/>
+    <div className="divider"/>
+    <Progress/>
+    <CompletedTasks/>
     </section>
     </div>
   )
