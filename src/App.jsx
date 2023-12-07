d49d95ae6ecc63bed14c43fde578720d22e8a335
B4g4rini <pedro.bagarini2000@gmail.com>
2023-12-07 11:26:09 -0300

    header finalizado Submit sendo realizado Crude sendo feito

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css"
+import CreateNewTask from "./components/CreateNewTask/CreateNewTask"
 import Header from "./components/Header/Header"
+
+
 
 const App = () => {
   return (
-    <>
+    <div className="container">
     <Header/>
-    </>
+    <section className="content">
+    <CreateNewTask/>
+
+    </section>
+    </div>
   )
 }
 
---
+++ src/components/CreateNewTask/CreateNewTask.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Title from "../Title/Title";
+import styles from "./CreateNewTask.module.css";
+import addIcon from "../../assets/icon-add.svg"
+
+const CreateNewTask = () => {
+  const[inputValue, setInputValue] = useState("")
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    inputValue.length ? setInputValue("") : null;
+    // if (inputValue.length){
+    //   setInputValue("")
+    // }
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+
+  }
+  return (
+    <div className={styles.container}>
+      <Title iconName={"pencil"} text={"Criar nova tarefa"} />
+      <form onSubmit={handleSubmit} className={styles.inputContainer}>
+        <input
+          className={styles.inputText}
+          type="text"
+          autoFocus
+          placeholder="Nome da tarefa"
+          value={inputValue}
+          onChange={handleChange}
+        />
+        <button className={styles.addButton} type="submit">
+          <img src={addIcon} alt="Adicionar tarefa"/>
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateNewTask;
